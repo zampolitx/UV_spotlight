d0569367c185e8903d97a526824831d74217368e
zampolitx <zampolitx_85@mail.ru>
2019-06-04 21:45:14 +0900

    Файл main.c main.h 1

--- UV_Spotlight_main.c
+++ UV_Spotlight_main.c
@@ -1,13 +1,20 @@
-/*
- * File:   UV_Spotlight_main.c
- * Author: zampolit
- *
- * Created on 2 ???? 2019 ?., 18:44
- */
-
-
 #include <xc.h>
+#include "UV_Spotlight_main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 void main(void) {
+    TRISA = 0b00000;
+    while(1)
+    {   
+        relay = 1;
+        __delay_ms(1000);
+        relay = 0;
+        __delay_ms(1000);
+        
+    }
     return;
 }
---
+++ UV_Spotlight_main.h
@@ -0,0 +1,19 @@
+#ifndef UV_SPOTLIGHT_MAIN_H
+#define UV_SPOTLIGHT_MAIN_H
+#include <xc.h>
+
+#define _XTAL_FREQ 20000000
+#define relay RA2
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#endif /* UV_SPOTLIGHT_MAIN_H */
+
